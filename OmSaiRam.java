bdab630ca3e894e3475427ca8a68a09af03f95c1
Bharath Kumar <bharatht336@gmail.com>
2023-03-19 13:02:13 +0530

    added another println statement

--- OmSaiRam.java
+++ OmSaiRam.java
@@ -1,5 +1,6 @@
 public class OmSaiRam{
    public static void main(String args[]){
       System.out.println("Hello god bless you with your programs");
+      System.out.println("added another println statement");
    }
 }
